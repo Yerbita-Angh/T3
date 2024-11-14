a6e0d6c42abc210945330eb3ed9d1bdbdcb18171
Yerbita-Angh <alejandronicolasgarcia01@gmail.com>
2024-11-14 08:39:41 -0500

    avance 2

--- CitaUtil.cs
+++ CitaUtil.cs
@@ -35,6 +35,30 @@ namespace CitasMedicas
 
             Console.WriteLine("Cita creada exitosamente.");
         }
+        public static void ListarCitas(List<Cita> citas)
+        {
+            Console.WriteLine("Listado de Citas:");
+            foreach (var cita in citas)
+            {
+                Console.WriteLine(cita);
+            }
+        }
 
+        public static void ModificarUniversidad(List<Cita> citas)
+        {
+            Console.Write("Ingrese el nombre de la universidad que desea buscar: ");
+            string universidadActual = Console.ReadLine();
+
+            Console.Write("Ingrese el nuevo nombre de la universidad: ");
+            string nuevaUniversidad = Console.ReadLine();
+
+            foreach (var cita in citas)
+            {
+                if (cita.Estudiante.Universidad.Equals(universidadActual, StringComparison.OrdinalIgnoreCase))
+                {
+                    cita.Estudiante.Universidad = nuevaUniversidad;
+                }
+            }
+        }
     }
 }
